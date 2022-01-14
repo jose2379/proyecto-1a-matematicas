29db7ee5c61ab872b6309ca969b2b39e0db26e7d
Jose María Miñambres Redondo <jose2379@gmail.com>
2022-01-14 09:51:29 +0100

    Primera versión del módulo de operaciones matemáticas simples

---
+++ index.js
@@ -0,0 +1,46 @@
+module.exports = {
+    /**
+     * Suma de números
+     * @example
+     * n1 = 1, n2 = 3 => resultado 4
+     * @param {*} n1 primer parámetro de la suma
+     * @param {*} n2 segundo parámetro de la suma
+     * @returns n1 + n2
+     */
+    suma: function (n1, n2) {
+        return n1 + n2;
+    },
+    /**
+     * Resta de números
+     * @example
+     * n1 = 3, n2 = 2 => resultado 1
+     * @param {*} n1 primer parámetro de la resta
+     * @param {*} n2 segundo parámetro de la resta
+     * @returns n1 - n2
+     */
+    resta: function (n1, n2) {
+        return n1 - n2;
+    },
+    /**
+     * Multiplicacion de números
+     * @example
+     * n1 = 4, n2 = 3 => resultado 12
+     * @param {*} n1 primer parámetro de la multiplicacion
+     * @param {*} n2 segundo parámetro de la multiplicacion
+     * @returns n1 * n2
+     */
+    multiplicacion: function (n1, n2) {
+        return n1 * n2;
+    },
+    /**
+     * Division de números
+     * @example
+     * n1 = 10, n2 = 5 => resultado 2
+     * @param {*} n1 primer parámetro de la division
+     * @param {*} n2 segundo parámetro de la division
+     * @returns n1 / n2
+     */
+    division: function (n1, n2) {
+        return n1 / n2;
+    }
+}
---
+++ pruebas.js
@@ -0,0 +1,5 @@
+const m = require('.');
+console.log(m.suma(3, 5));
+console.log(m.resta(3, 5));
+console.log(m.multiplicacion(3, 5));
+console.log(m.division(3, 5));
